ba95719d91a15ac4c5b4251913ad4ddda4e34e9d
Fred White <f.white@enian.co>
2021-07-29 09:39:57 +0100

    feat(temp): add fetch to get data to test api

--- packages/api/index.js
+++ packages/api/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const fs = require("fs");
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.get("/boat_ramps", (req, res) => {
   try {
--- packages/web/src/App.jsx
+++ packages/web/src/App.jsx
@@ -5,12 +5,24 @@ import './App.css';
 function App() {
   // Create the count state.
   const [count, setCount] = useState(0);
+  const [data, setData] = useState();
   // Create the counter (+1 every second).
   useEffect(() => {
     const timer = setTimeout(() => setCount(count + 1), 1000);
     return () => clearTimeout(timer);
   }, [count, setCount]);
   // Return the App component.
+  //
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:3000/boat_ramps');
+      const data = await response.json();
+      console.log(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
